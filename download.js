bc72e14ae8c133d995ea9f14bfdd246fab376846
Peter Wu <lekensteyn@gmail.com>
2012-12-06 00:12:18 +0100

    Change onreadystatechange to onload, re-indent
    Looks a scary commit, but it is changes only two functional lines
    (onreadystatechange to onload, remove readyState change check)

--- download.js
+++ download.js
@@ -6,30 +6,28 @@ function requestAsset(asset_query_base64) {
     xmlhttp.open("POST", "https://android.clients.google.com/market/api/ApiRequest", true);
     xmlhttp.responseType = "arraybuffer";
     xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-            if (this.status == 0) {
-                alert("ERROR:\n- Please disable SSL error warnings\n- Check your account information ( email and device ID )")
-            } else {
-                var respArr = new Uint8Array(this.response);
-                var data = "";
-                for (var i = 0; i < respArr.byteLength; i++) {
-                    var c = respArr[i];
-                    data += c < 32 || c > 122 ? "~" : String.fromCharCode(c);
-                }
-                if ((match = /(https?:\/\/[^:]+)/gi.exec(data))) {
-                    var baseURL = match[1];
-                    if ((match = /MarketDA.*?(\d+)/gi.exec(data))) {
-                        cookieValue = match[1];
-                        window.location.href = baseURL + "#" + cookieValue;
-                    } else {
-                        console.log("COOKIE: " + data);
-                        alert("ERROR: Cannot download this app!");
-                    }
+    xmlhttp.onload = function () {
+        if (this.status == 0) {
+            alert("ERROR:\n- Please disable SSL error warnings\n- Check your account information ( email and device ID )")
+        } else {
+            var respArr = new Uint8Array(this.response);
+            var data = "";
+            for (var i = 0; i < respArr.byteLength; i++) {
+                var c = respArr[i];
+                data += c < 32 || c > 122 ? "~" : String.fromCharCode(c);
+            }
+            if ((match = /(https?:\/\/[^:]+)/gi.exec(data))) {
+                var baseURL = match[1];
+                if ((match = /MarketDA.*?(\d+)/gi.exec(data))) {
+                    cookieValue = match[1];
+                    window.location.href = baseURL + "#" + cookieValue;
                 } else {
-                    console.log("HTTP: " + data);
+                    console.log("COOKIE: " + data);
                     alert("ERROR: Cannot download this app!");
                 }
+            } else {
+                console.log("HTTP: " + data);
+                alert("ERROR: Cannot download this app!");
             }
         }
     };
