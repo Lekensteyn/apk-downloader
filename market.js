3f7fd1e02bd2e8b9118228e81f5106775aa16a21
Rob Wu <gwnRob@gmail.com>
2013-07-27 15:44:57 +0200

    Fix market navigation detection, bg -> event page

--- market.js
+++ market.js
@@ -4,18 +4,42 @@
  * containing digits is non-free). To avoid breakage in the future, also show
  * the button if the price cannot be determined.
  */
-function triggerButtonVisibility() {
-    var price = document.querySelector("meta[itemprop=price]");
-    var showButton = !price || !/\d/.test(price.getAttribute("content"));
-    console.log("APK Downloader button will be " +
-            (showButton ? "shown" : "hidden"));
-    chrome.extension.sendMessage({
-        action: "showIcon",
-        show: showButton
-    });
+
+/* jshint browser:true */
+/* globals chrome, console, MutationObserver, WebKitMutationObserver */
+
+'use strict';
+var shownButton;
+function triggerButtonVisibilityCheck() {
+    var showButton = false;
+    if (location.pathname.lastIndexOf('/store/apps/details', 0) === 0) {
+        var price = document.querySelector("meta[itemprop=price]");
+        showButton = !price || !/\d/.test(price.getAttribute("content"));
+    }
+    if (showButton !== shownButton) {
+        console.log("APK Downloader button will be " +
+                (showButton ? "shown" : "hidden"));
+        chrome.extension.sendMessage({
+            action: "showIcon",
+            show: showButton
+        });
+        shownButton = showButton;
+    }
 }
 
-triggerButtonVisibility();
+if (typeof MutationObserver === 'undefined') { // Chrome 26-
+    window.MutationObserver = WebKitMutationObserver;
+}
+var observer = new MutationObserver(function() {
+    triggerButtonVisibilityCheck();
+});
+observer.observe(document.body, {
+    childList: true,
+    attributes: false,
+    characterData: false,
+    subtree: true
+});
+triggerButtonVisibilityCheck();
 
 chrome.extension.onMessage.addListener(function (message, sender, sendResponse) {
     if (message && message.action == "download") {
